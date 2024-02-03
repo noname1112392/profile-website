62c774257291d04e1aaaa327ac7f6c95cd36957d
noname1112392 <149623681+noname1112392@users.noreply.github.com>
2024-02-03 10:16:59 -0800

    Created function for tablist: Created three functions They are all the same When the tab is clicked, it will appear on the screen and the other two will be hidden

--- src/script.js
+++ src/script.js
@@ -3,6 +3,9 @@ const mobileMenu = document.getElementById('menu');
 const tab1 = document.getElementById('tab1');
 const tab2 = document.getElementById('tab2');
 const tab3 = document.getElementById('tab3');
+const content1 = document.getElementById('content1');
+const content2 = document.getElementById('content2');
+const content3 = document.getElementById('content3');
 
 mobileMenuBtn.addEventListener('click', navToggle);
 
@@ -14,3 +17,20 @@ function navToggle() {
 }
 
 // Tablist functions
+tab1.addEventListener('click', () => {
+    content1.removeAttribute('hidden');
+    content2.setAttribute('hidden', '');
+    content3.setAttribute('hidden', '');
+});
+
+tab2.addEventListener('click', () => {
+    content2.removeAttribute('hidden');
+    content1.setAttribute('hidden', '');
+    content3.setAttribute('hidden', '');
+});
+
+tab3.addEventListener('click', () => {
+    content3.removeAttribute('hidden');
+    content1.setAttribute('hidden', '');
+    content2.setAttribute('hidden', '');
+});
