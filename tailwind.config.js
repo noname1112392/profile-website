9289a8ea1ef380404785cda5eb7a1cd762e790c2
noname1112392 <149623681+noname1112392@users.noreply.github.com>
2024-02-05 20:16:05 -0800

    Edit screen size: Edit screen size manually in tailwind.config.js add screen size to each sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '414px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1920px',
+    }
   },
   plugins: [],
 }
